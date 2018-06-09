26175c694a730377c5a1fd84b275bbf7ce15b687
Michael Morris <mdam2345@gmail.com>
2018-06-09 03:33:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,11 +8,7 @@ def speak_to_grandma(phrase)
   end
 end
 
-if phrase === "I LOVE YOU GRANDMA"
-
-
-elsif  "I LOVE YOU GRANDMA!" 
-"I LOVE YOU TOO PUMPKIN!"
+=
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
