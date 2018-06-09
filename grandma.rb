bbed085ce73ac959191bcda18bde2afa1097c3d7
Michael Morris <mdam2345@gmail.com>
2018-06-09 02:26:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
    if phrase.downcase
       "HUH?! SPEAK UP, SONNY!"
    elsif phrase.upcase
-     puts "NO, NOT SINCE 1938!"
+      "NO, NOT SINCE 1938!"
    else phrase == "I LOVE YOU GRANDMA!"
      puts "I LOVE YOU TOO PUMPKIN!"
    end
